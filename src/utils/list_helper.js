70248475a14747a8f0a29e012922209cc0b68358
Anton Kravets <antonio.kravets@gmail.com>
2022-06-21 21:56:02 +0300

    4.3-4.7

--- src/utils/list_helper.js
+++ src/utils/list_helper.js
@@ -1,12 +1,39 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
 
 
 const totalLikes = (blogs) => {
-  return blogs.reduce((a, b) => {return  a + b.likes }, 0)
+  return blogs.reduce((a, b) => { return a + b.likes }, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((a, b) => { return a.likes > b.likes ? { title: a.title, author: a.author, likes: a.likes } : b })
+}
+
+const mostBlogs = (blogs) => {
+  const result = _(blogs)
+    .groupBy(x => x.author)
+    .map((value, key) => ({ author: key, blogs: value.length }))
+    .value()
+    .reduce((a, b) => { return a.likes > b.likes ? a : b })
+
+  return result
+
+}
+
+const mostLikes = (blogs) => {
+  const result =  _(blogs)
+    .groupBy(x => x.author)
+    .map((value, key) => ({ author: key, likes: _.sumBy(value, 'likes') }))
+    .value().reduce((a, b) => { return a.likes > b.likes ? a : b })
+
+  return result
+
 }
 
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
 }
